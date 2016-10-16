c8eef4d9146c8bfb74efdb2ae60884835797539b
Maulana Fadia Hadi <maulanafadia118@gmail.com>
2016-10-16 09:41:36 +0700

    StartActivityForResultFinal

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
@@ -1,13 +1,25 @@
 package id.sch.smktelkom_mlg.learn.intent2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonInput).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, InputActivity.class), REQUEST_CODE);
+            }
+        });
+
     }
 }
